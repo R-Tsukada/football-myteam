960a79b3e8b33db1bebf69ca45a9e479ff436659
R-Tsukada <r.tsukada14@gmail.com>
2022-03-17 21:35:24 +0900

    signin、signup後のページ遷移を設定
    singin、signup後にroot_pathへ遷移するように設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_up_path_for(resource)
+    root_path
+  end
+
   def after_sign_out_path_for(_resouce)
     new_user_session_path
   end
