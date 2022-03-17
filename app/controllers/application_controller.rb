474e214e712d2d68e4980629b3dc0e598ce5e609
R-Tsukada <r.tsukada14@gmail.com>
2022-03-17 21:38:09 +0900

    Lintの指摘を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     root_path
   end
 
-  def after_sign_up_path_for(resource)
+  def after_sign_up_path_for(_resource)
     root_path
   end
 
