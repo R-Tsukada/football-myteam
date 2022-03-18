ae4fbae03cc645b9897034d979411705d218ab85
R-Tsukada <r.tsukada14@gmail.com>
2022-03-18 10:17:23 +0900

    userモデルのテストを作成
    deviseのデフォルトに合わせて作成したテストなのでviewsを変更した場合、修正が必要になるかもしれない。

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,7 @@
 
 FactoryBot.define do
   factory :user do
+    email { 'admin@example.com' }
+    password { 'password' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a email, password' do
+    user = FactoryBot.create(:user)
+    expect(user).to be_valid
+  end
+
+  it 'is valid without a email' do
+    user = FactoryBot.build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it 'is valid with a duplicate email' do
+    FactoryBot.create(:user)
+    user = User.new(
+      email: 'admin@example.com',
+      password: '123456'
+    )
+    user.valid?
+    expect(user.errors[:email]).to include('has already been taken')
+  end
+
+  it 'is valid with a password' do
+    user = FactoryBot.build(:user, password: nil)
+    user.valid?
+    expect(user.errors[:password]).to include("can't be blank")
+  end
+
+  it 'is' do
+    user = User.new(
+      email: 'test@example.com',
+      password: '1234'
+    )
+    user.valid?
+    expect(user.errors[:password]).to include('is too short (minimum is 6 characters)')
+  end
 end
