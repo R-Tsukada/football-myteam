5bbe66d9f88e1b4ed723f2838ab69eb19ea10d73
R-Tsukada <r.tsukada14@gmail.com>
2022-03-18 10:15:36 +0900

    userモデルのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end
