b1ce0b17b7d731f974c4e4a8e4ab700463fe9980
R-Tsukada <r.tsukada14@gmail.com>
2022-03-21 18:04:20 +0900

    外部APIを使ってTeam情報を取得する機能を追加
    league_idの取得方法の検討が必要。

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,4 +5,61 @@ class Team < ApplicationRecord
   validates :home_city, presence: true
 
   belongs_to :league
+
+  require 'uri'
+  require 'net/http'
+  require 'openssl'
+  require 'json'
+
+  def api_request
+    team = Team.all
+    team.blank? ? team_data : return
+  end
+
+  def team_data
+    api_request_url.each do |n|
+      http = Net::HTTP.new(n.host, n.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      request = Net::HTTP::Get.new(n)
+      request['x-rapidapi-host'] = ENV['HOST']
+      request['x-rapidapi-key'] = ENV['KEY']
+
+      response = http.request(request)
+      results = JSON.parse(response.body)
+
+      results['response'].each_index do |result|
+        team = Team.new
+        team.api_id = results['response'][result]['team']['id']
+        team.name = results['response'][result]['team']['name']
+        team.logo = results['response'][result]['team']['logo']
+        team.home_city = results['response'][result]['venue']['city']
+        league = results['parameters']['league']
+        team.league_id = case league
+                          when '39'
+                            37
+                          when '78'
+                            38
+                          when '135'
+                            39
+                          when '140'
+                            40
+                          end
+        team.save
+      end
+    end
+  end
+
+  def api_request_url
+    league_numbers = [39, 135, 140, 78]
+    league_numbers.map { |number| URI("https://v3.football.api-sports.io/teams?league=#{number}&season=#{season_year}") }
+  end
+
+  def season_year
+    current_year = Time.zone.now.year
+    current_month = Time.zone.now.month
+    current_month <= 7 ? current_year - 1 : current_year
+  end
+
 end
