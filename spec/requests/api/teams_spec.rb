a8fea25d894a143a954ef45501bf07df105ac9e5
R-Tsukada <r.tsukada14@gmail.com>
2022-03-23 18:18:02 +0900

    APIのRequestテストを追加
    rails_helperに追加したコードはdeviseのsign_inメソッドを使えるようにするために追加した

---
+++ spec/requests/api/teams_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'API::Teams', type: :request do
+  describe "GET /api/teams/index" do
+    it 'returns http success' do
+      user = FactoryBot.build(:user)
+      sign_in user
+      get api_teams_path(format: :json)
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
