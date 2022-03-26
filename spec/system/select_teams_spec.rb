61855ba1cb66bc9f24873385ada831d159a21674
R-Tsukada <r.tsukada14@gmail.com>
2022-03-26 11:09:10 +0900

    リーグ一覧のシステムテストを追加
    Vueの表示に時間にかかるためsleepを追加している。
    v-ifメソッドでリーグが表示されているときはチーム一覧が表示されないようにするため、そのテストも追加している。

---
+++ spec/system/select_teams_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe 'SelectTeams', type: :system, js: true do
+  it 'showing league list', js: true do
+    using_wait_time(15) do
+      user = FactoryBot.create(:user)
+
+      visit root_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Log in'
+
+      expect(page).to have_content 'Signed in successfully.'
+
+      sleep 10.0
+
+      expect(page).to have_content 'Premier League'
+      expect(page).to have_content 'Serie A'
+      expect(page).to have_content 'La Liga'
+      expect(page).to have_content 'Bundesliga 1'
+      expect(page).to_not have_content 'Manchester United'
+    end
+  end
+end
