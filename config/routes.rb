c4ebcccafc890425f46be7707aea4a542ea76c24
R-Tsukada <r.tsukada14@gmail.com>
2022-04-03 15:38:14 +0900

    competitorsのルートを設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
     resources :leagues, only: [:index]
     resources :teams, only: [:index]
     resources :favorites, only: [:index, :create]
+    resources :competitors, only: [:index, :create]
   end
 end
