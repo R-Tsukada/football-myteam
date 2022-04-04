61ff54a1a3751f886bc29480c551b003edec44a6
R-Tsukada <r.tsukada14@gmail.com>
2022-04-04 17:32:43 +0900

    応援しているチームで登録したチームのidを渡せるようにした

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -4,9 +4,14 @@ class Api::FavoritesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   before_action :authenticate_user!
-  before_action :set_follow
+  before_action :set_follow, only: %i[create]
 
-  def index; end
+  def index
+    user = current_user
+    id = user.favorite.team_id
+    team = Team.find(id)
+    @favorite_team = team
+  end
 
   def create
     user = current_user
