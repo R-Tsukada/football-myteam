8c6fefaf12c0c2475eddfe2ac187bf9889502767
R-Tsukada <r.tsukada14@gmail.com>
2022-04-04 16:53:41 +0900

    Competitorsのリクエストテストを作成

---
+++ spec/requests/api/competitors_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "Api::Competitors", type: :request do
+  describe "post /index" do
+    it 'post http success' do
+      league = FactoryBot.create(:league)
+      team = FactoryBot.create(:team, league: league)
+      user = FactoryBot.create(:user)
+      sign_in user
+      post api_favorites_path, params: { id: team.id }
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
