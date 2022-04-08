2eb0cb97f3e96de7b2ff9c3ab54df1ef23719bc8
R-Tsukada <r.tsukada14@gmail.com>
2022-04-08 18:44:28 +0900

    バリデーションを追加

--- app/models/standing.rb
+++ app/models/standing.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class Standing < ApplicationRecord
+  validates :team_id, presence: true
+  validates :team_name, presence: true
+  validates :team_logo, presence: true
+  validates :rank, presence: true
+  validates :points, presence: true
+  validates :played, presence: true
 end
