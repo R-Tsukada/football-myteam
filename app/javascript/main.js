aba36bb8c720df22d2cafd593585699bb444c55b
R-Tsukada <r.tsukada14@gmail.com>
2022-04-11 00:05:50 +0900

    storeをグローバルに使えるようにした

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router/router.js'
+import { store } from './store/store.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   const selector = '#js-league-list'
@@ -8,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
   if (app) {
     const app = createApp(App)
     app.use(router)
+    app.use(store)
     app.mount(selector)
   }
 })
