f3a6b7152c9a22cfd871c6f13696189e02f36512
R-Tsukada <r.tsukada14@gmail.com>
2022-04-19 14:56:22 +0900

    試合予定をボーダーで囲んだ

--- app/javascript/components/table/FavoriteTeamTable.vue
+++ app/javascript/components/table/FavoriteTeamTable.vue
@@ -6,12 +6,16 @@
       <th>{{ standings.points }}</th>
       <th>{{ standings.played }}</th>
       <th v-for="match in matchSchedules" :key="match.id">
-        <img :src="match.competition_logo" class="image is-24x24" />
-        <p>{{ match.date }}</p>
-        <p>{{ match.home_and_away }}</p>
-        <br />
-        <img :src="match.team_logo" class="image is-48x48" />
-        <p>{{ match.team_name }}</p>
+        <div class="match_schedules">
+          <div>
+            <img :src="match.competition_logo" class="image is-24x24" />
+            <p>{{ match.home_and_away }}</p>
+          </div>
+          <div>
+            <p>{{ match.date }}</p>
+            <img :src="match.team_logo" class="image is-48x48" />
+          </div>
+        </div>
       </th>
     </tr>
   </tbody>
@@ -22,3 +26,11 @@ export default {
   props: ['standings', 'matchSchedules']
 }
 </script>
+
+<style>
+.match_schedules {
+  display: flex;
+  border: solid 1px;
+  border-radius: 8px;
+}
+</style>
