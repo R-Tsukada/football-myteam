820bd8279e4e423807fd23452635cc6f42d3a87b
R-Tsukada <r.tsukada14@gmail.com>
2022-04-21 15:20:10 +0900

    詳細ページを追加

---
+++ app/javascript/components/list/MatchResultList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex">
+    <ul class="flex-list" v-for="result in matchResultFilter" :key="result.id">
+      <li>
+        <div class="competition">
+          <p>{{ result.date }}</p>
+          <img :src="result.competition_logo" class="image is-64x64" />
+          <p>{{ result.competition_name }}</p>
+        </div>
+        <div class="team">
+          <p class="home_and_away">{{ result.home_and_away }}</p>
+          <p>{{ result.home_team_name }}</p>
+          <img :src="result.home_logo"  class="image is-96x96"/>
+          <p>{{ result.home_score }}</p>
+          <p> - </p>
+          <p>{{ result.away_score }}</p>
+          <img :src="result.away_logo" class="image is-96x96"/>
+          <p>{{ result.away_team_name }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>>
+
+<script>
+export default {
+  props: ['matchResultFilter']
+}
+</script>
