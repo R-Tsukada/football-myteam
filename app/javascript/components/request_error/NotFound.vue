ffabf27152c9cc58e79b3a19b9d2fc9776a91c96
R-Tsukada <r.tsukada14@gmail.com>
2022-05-03 23:20:51 +0900

    404ページを作成
    日程一覧に戻るボタンを設置した。
    トップページに戻るボタンの設置も検討する。

---
+++ app/javascript/components/request_error/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class='container'>
+    <div class='box'>
+      <h1 class='is-size-1 has-text-centered has-text-weight-bold'>
+        404
+      </h1>
+      <h2 class='is-size-3 has-text-centered py-2'>
+        お探しのページが見つかりませんでした
+      </h2>
+      <p class='py-2'>ご指定のページは削除されたか、移動した可能性があります</p>
+    </div>
+    <button class='button is-rounded is-medium mx-auto'>
+      <router-link to='/schedules'>日程一覧に戻る</router-link>
+    </button>
+  </div>
+</template>
