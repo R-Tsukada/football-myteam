7237a56c569366aaff147110b4a2592089868baa
R-Tsukada <r.tsukada14@gmail.com>
2022-05-05 22:46:56 +0900

    ログインした状態が継続している場合、自動的に日程表にリダイレクトされるようにした

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def index; end
+  def index
+    redirect_to '/schedules' if current_user
+  end
 
   def privacy_policy; end
 
