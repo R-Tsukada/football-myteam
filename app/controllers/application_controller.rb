32228fb64727ae982e5c22f1b5ac5782f2d12323
R-Tsukada <r.tsukada14@gmail.com>
2022-05-05 15:55:02 +0900

    ログイン後は日程一覧にページ遷移するように設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def after_sign_in_path_for(_resource)
-    leagues_path
+    '/schedules'
   end
 
   def after_sign_up_path_for(_resource)
