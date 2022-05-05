f0db1a089f802573896114f48c731867e60365f2
R-Tsukada <r.tsukada14@gmail.com>
2022-05-05 13:52:08 +0900

    ライバルチームが3チーム以下の場合の処理

--- app/controllers/api/secound_competitor_team_matches_controller.rb
+++ app/controllers/api/secound_competitor_team_matches_controller.rb
@@ -5,6 +5,10 @@ class Api::SecoundCompetitorTeamMatchesController < ApplicationController
     user = current_user
     competitor_teams = user.competitor.map(&:team_id)
     current_team = Team.find(competitor_teams[1]).api_id
-    @secound_competitor_team_matches = Match.all.order(:date).where(date: Time.zone.today.., team_matches_index: current_team).first(3)
+    if competitor_teams.length >= 2
+      @secound_competitor_team_matches = Match.all.order(:date).where(date: Time.zone.today.., team_matches_index: current_team).first(3)
+    else
+      return render json: { message: 'error' }, status: :unprocessable_entity
+    end
   end
 end
--- app/controllers/api/third_competitor_team_matches_controller.rb
+++ app/controllers/api/third_competitor_team_matches_controller.rb
@@ -4,7 +4,11 @@ class Api::ThirdCompetitorTeamMatchesController < ApplicationController
   def index
     user = current_user
     competitor_teams = user.competitor.map(&:team_id)
-    current_team = Team.find(competitor_teams[2]).api_id
-    @third_competitor_team_matches = Match.all.order(:date).where(date: Time.zone.today.., team_matches_index: current_team).first(3)
+    if competitor_teams.length == 3
+      current_team = Team.find(competitor_teams[2]).api_id
+      @third_competitor_team_matches = Match.all.order(:date).where(date: Time.zone.today.., team_matches_index: current_team).first(3)
+    else
+      return render json: { message: 'error' }, status: :unprocessable_entity
+    end
   end
 end
