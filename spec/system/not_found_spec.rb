fdbd92d5df50caa981e22f1c0352be212aa2e4c9
R-Tsukada <r.tsukada14@gmail.com>
2022-05-05 22:35:36 +0900

    テストを修正

--- spec/system/not_found_spec.rb
+++ spec/system/not_found_spec.rb
@@ -4,13 +4,13 @@ require 'rails_helper'
 
 RSpec.describe 'NotFoundPage', type: :system, js: true do
   it 'display 404 page', js: true do
-    user = FactoryBot.create(:user)
-
     visit root_path
-    all('.button')[1].click_link 'ログイン'
-    fill_in 'Eメール', with: user.email
-    fill_in 'パスワード', with: user.password
-    click_button 'ログイン'
+
+    first('.button').click_link 'アカウント作成'
+    fill_in 'Eメール', with: 'abc@example.com'
+    fill_in 'パスワード' , with: '123456'
+    fill_in 'パスワード（確認用）' , with: '123456'
+    click_button 'アカウント登録'
 
     visit '/rails'
     expect(page).to have_content "404\nお探しのページが見つかりませんでした"
--- spec/system/signin_and_signup_spec.rb
+++ spec/system/signin_and_signup_spec.rb
@@ -2,37 +2,55 @@
 
 require 'rails_helper'
 
-RSpec.describe 'SelectTeams', type: :system, js: true do
+RSpec.describe 'devise', type: :system, js: true do
+  before do
+    @user = FactoryBot.create(:user)
+    @league = FactoryBot.create(:league)
+    @arsenal = Team.create(
+      id: 1,
+      name: 'Arsenal',
+      logo: 'https://media.api-sports.io/football/teams/42.png',
+      api_id: '42',
+      home_city: 'London',
+      league_id: @league.id
+    )
+    @manchester = Team.create(
+      id: 2,
+      name: 'Manchester United',
+      logo: 'https://media.api-sports.io/football/teams/33.png',
+      api_id: '33',
+      home_city: 'Manchester',
+      league_id: @league.id
+    )
+  end
+
   it 'user can login', js: true do
-    user = FactoryBot.create(:user)
-    league = FactoryBot.create(:league)
+    FactoryBot.create(:favorite, user: @user, team: @arsenal)
+    @user.competitor_team_follow(@manchester)
 
     visit root_path
     all('.button')[1].click_link 'ログイン'
-    fill_in 'Eメール', with: user.email
-    fill_in 'パスワード', with: user.password
+    fill_in 'Eメール', with: @user.email
+    fill_in 'パスワード', with: @user.password
     click_button 'ログイン'
 
     expect(page).to have_content 'ログインしました'
-    expect(page).to have_content league.name
   end
 
   it 'email error when login.', js: true do
-    user = FactoryBot.create(:user)
     visit root_path
     all('.button')[1].click_link 'ログイン'
     fill_in 'Eメール', with: 'error@example.com'
-    fill_in 'パスワード', with: user.password
+    fill_in 'パスワード', with: @user.password
     click_button 'ログイン'
 
     expect(page).to have_content 'Eメールまたはパスワードが違います。'
   end
 
   it 'password error when login.', js: true do
-    user = FactoryBot.create(:user)
     visit root_path
     all('.button')[1].click_link 'ログイン'
-    fill_in 'Eメール', with: user.email
+    fill_in 'Eメール', with: @user.email
     fill_in 'パスワード', with: 'xxxxxx'
     click_button 'ログイン'
 
@@ -40,7 +58,6 @@ RSpec.describe 'SelectTeams', type: :system, js: true do
   end
 
   it 'user create new account', js: true do
-    league = FactoryBot.create(:league)
     visit root_path
     first('.button').click_link 'アカウント作成'
     fill_in 'Eメール', with: 'abc@example.com'
@@ -49,7 +66,7 @@ RSpec.describe 'SelectTeams', type: :system, js: true do
     click_button 'アカウント登録'
 
     expect(page).to have_content 'アカウント登録が完了しました'
-    expect(page).to have_content league.name
+    expect(page).to have_content @league.name
   end
 
   it 'password validation enabled during account creation', js: true do
