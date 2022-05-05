e00a8d655de8d26930164ad51f30715b5e3ae9ce
R-Tsukada <r.tsukada14@gmail.com>
2022-05-05 22:48:11 +0900

    lintの指摘を修正

--- spec/system/not_found_spec.rb
+++ spec/system/not_found_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe 'NotFoundPage', type: :system, js: true do
 
     first('.button').click_link 'アカウント作成'
     fill_in 'Eメール', with: 'abc@example.com'
-    fill_in 'パスワード' , with: '123456'
-    fill_in 'パスワード（確認用）' , with: '123456'
+    fill_in 'パスワード', with: '123456'
+    fill_in 'パスワード（確認用）', with: '123456'
     click_button 'アカウント登録'
 
     visit '/rails'
