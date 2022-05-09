482ce5179c2a6b8dd85ae3201f4e59ac9ff49127
R-Tsukada <r.tsukada14@gmail.com>
2022-05-09 22:05:26 +0900

    応援しているチームを登録してあればそのチームに背景色がつき、なにも登録していなければリーグ情報飲みが表示されるようにした

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -4,13 +4,12 @@ class Api::FavoritesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   before_action :authenticate_user!
+  before_action :set_favorite_team, only: %i[index]
   before_action :set_follow, only: %i[create]
 
   def index
     user = current_user
-    id = user.favorite.team_id
-    team = Team.find(id)
-    @favorite_team = team
+    @favorite_team = user.favorite.present? ? @team : @not_team
   end
 
   def create
@@ -19,6 +18,16 @@ class Api::FavoritesController < ApplicationController
   end
 
   private
+
+  def set_favorite_team
+    user = current_user
+    if user.favorite.present?
+      id = user.favorite.team_id
+      @team = Team.find(id)
+    else
+      @not_team = []
+    end
+  end
 
   def set_follow
     id = params.require(:favorite).permit(:id)
