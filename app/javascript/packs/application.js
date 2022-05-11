3a799ba37a297885e02cb7d13a3786e5fe3dda45
R-Tsukada <r.tsukada14@gmail.com>
2022-05-11 12:09:23 +0900

    Turbolinksを削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,12 +4,10 @@
 // that code so it'll be compiled.
 
 import Rails from '@rails/ujs'
-import Turbolinks from 'turbolinks'
 import * as ActiveStorage from '@rails/activestorage'
 import 'channels'
 
 require('../main.js')
 
 Rails.start()
-Turbolinks.load()
 ActiveStorage.start()
