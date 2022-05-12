203ebc1df3dea31cc4d6d2837c0eeae8085f8e2e
R-Tsukada <r.tsukada14@gmail.com>
2022-05-12 16:52:29 +0900

    テストデータを追加

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -16,12 +16,28 @@ FactoryBot.define do
       home_city { 'London' }
     end
 
-    trait :Manchester_United do
+    trait :manchester_united do
       id { 2 }
       name { 'Manchester United' }
       logo { 'https://media.api-sports.io/football/teams/33.png' }
       api_id { 33 }
       home_city { 'Manchester' }
+    end
+
+    trait :lazio do
+      id { 3 }
+      name { 'Lazio' }
+      logo { 'https://media.api-sports.io/football/teams/487.png' }
+      api_id { 487 }
+      home_city { 'Roma' }
+    end
+
+    trait :sassuolo do
+      id { 4 }
+      name { 'Sassuolo' }
+      logo { 'https://media.api-sports.io/football/teams/488.png' }
+      api_id { 488 }
+      home_city { 'Reggio' }
     end
   end
 end
