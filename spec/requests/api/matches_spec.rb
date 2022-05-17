f4eef7c9c52decb2fa759fc45e66d7de145ad38d
R-Tsukada <r.tsukada14@gmail.com>
2022-05-17 17:06:02 +0900

    コントローラーの変更に合わせてテストを修正

--- spec/requests/api/matches_spec.rb
+++ spec/requests/api/matches_spec.rb
@@ -2,15 +2,15 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Api::FavoriteTeamMatches', type: :request do
-  describe 'GET /api/favorite_team_matches/index' do
+RSpec.describe 'Api::Matches', type: :request do
+  describe 'GET /api/matches/index' do
     it 'returns http success' do
       league = FactoryBot.create(:league)
       team = FactoryBot.create(:team, league: league)
       user = FactoryBot.build(:user)
       sign_in user
       user.favorite_team_follow(team)
-      get api_favorite_team_matches_path(format: :json)
+      get api_matches_path(format: :json)
       expect(response).to have_http_status(:success)
     end
   end
