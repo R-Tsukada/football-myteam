73704cb3f1a8b9be2f318b02aa21c1a98c347b3f
R-Tsukada <r.tsukada14@gmail.com>
2022-06-16 15:58:19 +0900

    リファクタリングを実施

--- app/controllers/api/team_filter_controller.rb
+++ app/controllers/api/team_filter_controller.rb
@@ -4,10 +4,7 @@ class API::TeamFilterController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    user = current_user
-    id = user.favorite.team_id
-    team = Team.find(id)
-    league = team.league_id
-    @filter = Team.where(league_id: league).where.not(id: team.id)
+    team = current_user.favorite.team
+    @filter = Team.where(league: team.league_id).where.not(id: team)
   end
 end
