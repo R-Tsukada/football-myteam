05878ed99e9c5d6be4e610a048fb2b2849c8e2bf
R-Tsukada <r.tsukada14@gmail.com>
2022-06-16 15:28:06 +0900

    パスの書き方を変更
    ヘルパーメソッドを使うようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def after_sign_in_path_for(_resource)
-    '/schedules'
+    root_path
   end
 
   def after_sign_out_path_for(_resouce)
