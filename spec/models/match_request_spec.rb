5ae92d20f817e0939ecb8cb3f4553208bad57d28
R-Tsukada <r.tsukada14@gmail.com>
2022-08-04 23:40:25 +0900

    テストをリファクタリングした

--- spec/models/match_request_spec.rb
+++ spec/models/match_request_spec.rb
@@ -3,30 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe MatchRequest, type: :model do
-  let(:user) { FactoryBot.create(:user) }
-  let(:league) { FactoryBot.build(:league, :premier_league) }
-  let(:arsenal) { FactoryBot.build(:team, :arsenal, league: league) }
-  let(:manchester_united) { FactoryBot.build(:team, :manchester_united, league: league) }
   let(:favorite_url) { URI('https://v3.football.api-sports.io/fixtures?&season=2021&team=42&from=2022-02-30&to=2022-03-30') }
   let(:competitor_url) do
     URI('https://v3.football.api-sports.io/fixtures?&season=2021&team=33&from=2022-02-30&to=2022-03-30')
   end
   let(:registered_teames) { [favorite_url, competitor_url] }
-
-  before do
-    arsenal
-    manchester_united
-  end
+  let(:matches) { FactoryBot.build(:match) }
 
   it 'is ensure that the MatchRequest.league method is executed' do
-    matches = FactoryBot.build(:match)
     api_matche_mock = double(matches)
     allow(MatchRequest).to receive(:league).and_return(api_matche_mock)
     expect { MatchRequest.league(registered_teames) }.not_to raise_error
   end
 
   it 'is ensure that the MatchRequest.save_match method is executed' do
-    matches = FactoryBot.build(:match)
     api_match_mock = double(matches)
     allow(MatchRequest).to receive(:create).and_return(api_match_mock)
     expect { MatchRequest.create(registered_teames) }.not_to raise_error
--- spec/models/standing_request_spec.rb
+++ spec/models/standing_request_spec.rb
@@ -3,28 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe StandingRequest, type: :model do
-  let(:user) { FactoryBot.create(:user) }
-  let(:league) { FactoryBot.build(:league, :premier_league) }
-  let(:arsenal) { FactoryBot.build(:team, :arsenal, league: league) }
-  let(:manchester_united) { FactoryBot.build(:team, :manchester_united, league: league) }
+  let(:league) { FactoryBot.build(:league) }
   let(:favorite_url) { URI("https://v3.football.api-sports.io/standings?league=#{league.api_id}&season=2021&team=42") }
   let(:competitor_url) { URI("https://v3.football.api-sports.io/standings?league=#{league.api_id}&season=2021&team=33") }
   let(:registered_teames) { [favorite_url, competitor_url] }
-
-  before do
-    arsenal
-    manchester_united
-  end
+  let(:standings) { FactoryBot.build(:standing) }
 
   it 'is ensure that the StandingRequest.request_response method is executed' do
-    standings = FactoryBot.build(:standing)
     standings_mock = double(standings)
     allow(StandingRequest).to receive(:league).and_return(standings_mock)
     expect { StandingRequest.league(registered_teames) }.not_to raise_error
   end
 
   it 'is ensure that the StandingRequest.save_standing method is executed' do
-    standings = FactoryBot.build(:standing)
     standings_mock = double(standings)
     allow(StandingRequest).to receive(:create).and_return(standings_mock)
     expect { StandingRequest.create(registered_teames) }.not_to raise_error
