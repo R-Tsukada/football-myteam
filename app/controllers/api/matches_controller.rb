6dc97100bdb7aeabe2e5e70d756576174e950bba
R-Tsukada <r.tsukada14@gmail.com>
2022-08-08 11:53:40 +0900

    lintの指摘を修正

--- app/controllers/api/matches_controller.rb
+++ app/controllers/api/matches_controller.rb
@@ -25,7 +25,9 @@ class API::MatchesController < ApplicationController
 
   def api_request_url
     api_id = competitor_teams.unshift(current_user.favorite.team.api_id)
-    api_id.map { |i| URI("https://v3.football.api-sports.io/fixtures?&season=#{Year.season}&team=#{i}&from=#{Time.zone.now.prev_month.strftime('%Y-%m-%d')}&to=#{Time.zone.now.next_month.strftime('%Y-%m-%d')}") }
+    api_id.map do |i|
+      URI("https://v3.football.api-sports.io/fixtures?&season=#{Year.season}&team=#{i}&from=#{Time.zone.now.prev_month.strftime('%Y-%m-%d')}&to=#{Time.zone.now.next_month.strftime('%Y-%m-%d')}")
+    end
   end
 
   def match_date
