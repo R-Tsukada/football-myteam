be8e2bc7b8c329da507b49fb635980da508575f9
R-Tsukada <r.tsukada14@gmail.com>
2022-08-08 12:15:02 +0900

    チーム選択の手順の背景色を変更

--- app/javascript/components/page/TeamSelect.vue
+++ app/javascript/components/page/TeamSelect.vue
@@ -10,7 +10,7 @@
       <p class="is-size-4-tablet has-text-centered has-text-weight-semibold">
         チーム選びの手順
       </p>
-      <ol class="p-2 is-size-6-mobile">
+      <ol class="p-2 is-size-6-mobile has-text-centered">
         <li class="p-1">あなたが応援しているチームを選びます。</li>
         <li class="p-1">
           1で選んだのと同じリーグの中からライバルチームを選びます。
