f34cc97f5722757136651a1a57bce0ec9b33980f
R-Tsukada <r.tsukada14@gmail.com>
2022-08-15 16:16:08 +0900

    StandingとMatchが空だった場合はAPIリクエストをする

--- app/controllers/api/matches_controller.rb
+++ app/controllers/api/matches_controller.rb
@@ -2,6 +2,7 @@
 
 class API::MatchesController < ApplicationController
   before_action :set_show_page, only: [:show]
+  before_action :match_api_request
 
   def index
     @match = Match.all.order(:date).where(date: Time.zone.today..)
@@ -15,5 +16,25 @@ class API::MatchesController < ApplicationController
 
   def set_show_page
     @team_id = Team.find(params[:id])
+  end
+
+  def match_api_request
+    set_match if Match.all.blank?
+  end
+
+  def set_match
+    Match.delete_all
+    MatchRequest.league(api_request_url)
+  end
+
+  def api_request_url
+    api_id = competitor_teams.unshift(current_user.favorite.team.api_id)
+    api_id.map do |i|
+      URI("https://v3.football.api-sports.io/fixtures?&season=#{Year.season}&team=#{i}&from=#{Time.zone.now.prev_month.strftime('%Y-%m-%d')}&to=#{Time.zone.now.next_month.strftime('%Y-%m-%d')}")
+    end
+  end
+
+  def competitor_teams
+    SelectTeam.competitor(current_user)
   end
 end
--- app/controllers/api/standings_controller.rb
+++ app/controllers/api/standings_controller.rb
@@ -2,10 +2,40 @@
 
 class API::StandingsController < ApplicationController
   before_action :authenticate_user!
+  before_action :api_request
 
   def index
     @standing = Standing.all
   end
 
   def show; end
+
+  private
+
+  def api_request
+    set_standing if Standing.all.blank?
+  end
+
+  def set_standing
+    Standing.delete_all
+    StandingRequest.league(api_request_url)
+  end
+
+  def api_request_url
+    team_numbers = competitor_team_api_id.unshift(favorite_team.api_id)
+    team_numbers.map { |number| URI("https://v3.football.api-sports.io/standings?league=#{league_api_id(favorite_team)}&season=#{Year.season}&team=#{number}") }
+  end
+
+  def competitor_team_api_id
+    SelectTeam.competitor(current_user)
+  end
+
+  def league_api_id(favorite_team)
+    league = League.find(favorite_team.league_id)
+    league.api_id
+  end
+
+  def favorite_team
+    current_user.favorite.team
+  end
 end
