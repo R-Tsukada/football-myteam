fa8874f5019cf6e837bf0fcb732756df5e285fdc
R-Tsukada <r.tsukada14@gmail.com>
2022-08-17 23:06:17 +0900

    試合結果のスコアのpaddingを削除

--- app/javascript/components/page/TeamSchedule/list/MatchResultList.vue
+++ app/javascript/components/page/TeamSchedule/list/MatchResultList.vue
@@ -31,20 +31,20 @@
           class="column m-auto has-text-centered has-text-weight-bold is-size-3-tablet mobile-display">
           {{ result.home_team_name }}
         </p>
-        <div class="column m-auto schedule-column-logo">
+        <div class="column schedule-column-logo m-auto">
           <img :src="result.home_logo" class="image team-logo m-auto" />
         </div>
         <!-- schedule-column-logo -->
         <p
-          class="column has-text-centered has-text-weight-bold is-size-1 is-size-3-mobile m-auto">
+          class="column schedule-column-score has-text-centered has-text-weight-bold is-size-1 is-size-3-mobile m-auto">
           {{ result.home_score }}
         </p>
         <p
-          class="column has-text-centered has-text-weight-bold is-size-3 is-size-5-mobile m-auto">
+          class="column schedule-column-score has-text-centered has-text-weight-bold is-size-3 is-size-5-mobile m-auto">
           -
         </p>
         <p
-          class="column has-text-centered has-text-weight-bold is-size-1 is-size-3-mobile m-auto">
+          class="column schedule-column-score has-text-centered has-text-weight-bold is-size-1 is-size-3-mobile m-auto">
           {{ result.away_score }}
         </p>
         <div class="column m-auto schedule-column-logo">
