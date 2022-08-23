e0455faadc03f29f717987b49b645db44beaa1ca
R-Tsukada <r.tsukada14@gmail.com>
2022-08-23 09:10:20 +0900

    DBを修正

--- db/schema.rb
+++ db/schema.rb
@@ -100,7 +100,6 @@ ActiveRecord::Schema.define(version: 2022_08_22_070413) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "provider"
     t.string "uid"
-    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
