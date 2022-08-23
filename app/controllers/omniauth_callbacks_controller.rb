cddadf5bf98ba0b04848a963c590d23c5cf446c9
R-Tsukada <r.tsukada14@gmail.com>
2022-08-23 14:50:33 +0900

    twitter認証後のリダイレクト先を修正

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,13 +1,12 @@
 class OmniauthCallbacksController < ApplicationController
-  # skip_before_action :verify_authenticity_token, only: :twitter
 
   def twitter
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user
+      sign_in_and_redirect @user, event: :authentication
     else
-      sign_in_and_redirect leagues_path
+      sign_in_and_redirect new_user_registration_url
     end
   end
 
