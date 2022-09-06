103b9a7054c5894985267a820da1c08f4186fd2f
R-Tsukada <r.tsukada14@gmail.com>
2022-09-06 16:49:48 +0900

    404ページのボタンをコンポーネントに変更 & レイアウトを修正

--- app/javascript/components/request_error/NotFound.vue
+++ app/javascript/components/request_error/NotFound.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="container">
-    <div class="box">
-      <h1 class="is-size-1 has-text-centered has-text-weight-bold">404</h1>
-      <h2 class="is-size-3 has-text-centered py-2">
-        お探しのページが見つかりませんでした
-      </h2>
-      <p class="py-2">ご指定のページは削除されたか、移動した可能性があります</p>
+    <div class="section has-text-centered">
+      <div class="box">
+        <h1 class="is-size-1 has-text-weight-bold">404</h1>
+        <h2 class="is-size-3 py-2">お探しのページが見つかりませんでした</h2>
+        <p class="py-2">
+          ご指定のページは削除されたか、移動した可能性があります
+        </p>
+      </div>
+      <router-link to="/schedules">
+        <BackToPageButton class="is-rounded" label="日程一覧に戻る" />
+      </router-link>
     </div>
-    <button class="button is-rounded is-medium mx-auto">
-      <router-link to="/schedules">日程一覧に戻る</router-link>
-    </button>
   </div>
 </template>
+<script>
+import BackToPageButton from '../atoms/Button/BackToPageButton.vue'
+
+export default {
+  components: {
+    BackToPageButton
+  }
+}
+</script>
