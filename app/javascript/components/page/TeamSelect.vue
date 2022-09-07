d3cf7a055637b7e4d3784d1ebf5b73b90b3b19c8
R-Tsukada <r.tsukada14@gmail.com>
2022-09-07 17:56:10 +0900

    チーム選択の手順をコンポーネントにした

--- app/javascript/components/page/TeamSelect.vue
+++ app/javascript/components/page/TeamSelect.vue
@@ -5,23 +5,7 @@
         class="has-text-centered is-size-2-tablet is-size-6-mobile has-text-weight-bold">
         応援しているチームを選んでください
       </h2>
-      <div
-        class="box team-select-description mt-3 mx-auto"
-        v-if="!data.competitors.length">
-        <p class="is-size-4-tablet has-text-centered has-text-weight-semibold">
-          チーム選びの手順
-        </p>
-        <ol class="p-2 is-size-6-mobile has-text-centered">
-          <li class="p-1">あなたが応援しているチームを選びます。</li>
-          <li class="p-1">
-            1で選んだのと同じリーグの中からライバルチームを選びます。
-          </li>
-          <li class="p-1">
-            まずは応援しているチームが所属しているリーグを選んでください。
-          </li>
-        </ol>
-      </div>
-      <!-- box -->
+      <TeamSelectDescription v-if="!data.competitors.length" />
       <div
         class="has-text-right is-size-6-tablet is-size-7-mobile has-text-weight-bold mt-3"
         v-else>
@@ -105,12 +89,14 @@ import { reactive, onMounted, computed } from 'vue'
 import LeagueListLoader from '../loader/LeagueListLoader'
 import TeamListLoader from '../loader/TeamListLoader'
 import FavoriteSelectButton from '../Molecules/BooleanButton/FavoriteSelectButton.vue'
+import TeamSelectDescription from '../Molecules/TeamSelectDescription.vue'
 
 export default {
   components: {
     TeamListLoader,
     LeagueListLoader,
-    FavoriteSelectButton
+    FavoriteSelectButton,
+    TeamSelectDescription
   },
   setup() {
     const data = reactive({
