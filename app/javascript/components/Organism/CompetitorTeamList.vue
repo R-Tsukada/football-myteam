50b083f079c597927689ce323d441f69d130e3fd
R-Tsukada <r.tsukada14@gmail.com>
2022-09-09 06:56:50 +0900

    自由にライバルチームを選ぶときのチームリストをコンポーネント化した
    応援しているチームを選ぶときのチームリストと同じコンポーネントにしたかったが、v-bindを共通化することができなかった。
    対応方法を検討していく。

---
+++ app/javascript/components/Organism/CompetitorTeamList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="columns is-mobile is-flex-wrap-wrap has-text-centered">
+    <div
+      class="column is-one-fifth-tablet is-one-third-mobile has-text-centered"
+      v-for="team in teams"
+      :key="team.id">
+      <div
+        class="card has-hover-action select-button"
+        @click="selectTeam(team)"
+        v-bind:class="{
+          'has-background-link-light is-selected': competitors.some(
+            (competitor) => competitor.team_id === team.id
+          )
+        }">
+        <img :src="team.logo" class="image team-logo mx-auto" />
+        <p
+          class="has-text-weight-semibold mt-2 is-size-6-tablet is-size-7-mobile is-break-all">
+          {{ team.name }}
+        </p>
+      </div>
+      <!-- card -->
+    </div>
+    <!-- column -->
+  </div>
+  <!-- columns -->
+</template>
+<script>
+export default {
+  props: ['teams', 'competitors'],
+  setup(props, context) {
+    const selectTeam = (team) => {
+      context.emit('selectTeam', team)
+    }
+    return {
+      selectTeam
+    }
+  }
+}
+</script>
--- app/javascript/components/page/CompetitorTeamSelect/CompetitorTeamSelect.vue
+++ app/javascript/components/page/CompetitorTeamSelect/CompetitorTeamSelect.vue
@@ -123,30 +123,10 @@
       <CompetitorValidation v-else />
       <TeamListLoader v-if="!data.teams.length" />
       <div v-else>
-        <div class="columns is-mobile is-flex-wrap-wrap has-text-centered">
-          <div
-            class="column is-one-fifth-tablet is-one-third-mobile has-text-centered"
-            v-for="team in data.teams"
-            :key="team.id">
-            <div
-              class="card has-hover-action select-button"
-              @click="followTeam(team)"
-              v-bind:class="{
-                'has-background-link-light is-selected': data.competitors.some(
-                  (competitor) => competitor.team_id === team.id
-                )
-              }">
-              <img :src="team.logo" class="image team-logo mx-auto" />
-              <p
-                class="has-text-weight-semibold mt-2 is-size-6-tablet is-size-7-mobile is-break-all">
-                {{ team.name }}
-              </p>
-            </div>
-            <!-- card -->
-          </div>
-          <!-- column -->
-        </div>
-        <!-- columns -->
+        <TeamList
+          :teams="data.teams"
+          :competitors="data.competitors"
+          @selectTeam="followTeam" />
       </div>
       <!-- v-else -->
       <div class="back-or-select-buttons mt-4">
@@ -172,6 +152,7 @@ import BackToPageButton from '../../atoms/Button/BackToPageButton.vue'
 import BaseButton from '../../atoms/Button/BaseButton.vue'
 import DetermineButton from '../../atoms/Button/DetermineButton'
 import CompetitorSelectButton from '../../Molecules/BooleanButton/CompetitorSelectButton'
+import TeamList from '../../Organism/CompetitorTeamList.vue'
 
 export default {
   components: {
@@ -181,7 +162,8 @@ export default {
     BackToPageButton,
     BaseButton,
     DetermineButton,
-    CompetitorSelectButton
+    CompetitorSelectButton,
+    TeamList
   },
   setup() {
     const data = reactive({
