8fcee1a7b40bb6e012087828732274260ea260cb
R-Tsukada <r.tsukada14@gmail.com>
2022-09-09 11:11:28 +0900

    条件指定したチーム表示をコンポーネン化した

--- app/javascript/components/Molecules/CompetitorTeamSelectHomeOrRank.vue
+++ app/javascript/components/Molecules/CompetitorTeamSelectHomeOrRank.vue
@@ -6,37 +6,10 @@
     <section>
       <CompetitorTeamCount :competitors="competitors" v-if="isShowingMessage" />
       <CompetitorValidation v-else />
-      <div class="columns is-multiline is-mobile">
-        <div class="column mx-auto" v-for="team in teams" :key="team.id">
-          <div
-            class="card has-hover-action select-button"
-            @click="followTeam(team)"
-            v-bind:class="{
-              'has-background-link-light is-selected': competitors.some(
-                (competitor) => competitor.team_id === team.id
-              )
-            }">
-            <img
-              :src="team.logo"
-              class="image competitor-team-logo mx-auto pt-1" />
-            <p
-              class="has-text-weight-medium mt-2 is-size-2-tablet is-size-7-mobile">
-              {{ team.name }}
-            </p>
-            <p class="has-text-weight-medium is-size-3-tablet is-size-5-mobile">
-              <span class="is-size-5-tablet is-size-7-mobile">21-22:</span
-              >{{ team.last_season_rank }}<span>位</span>
-            </p>
-            <p class="has-text-weight-medium is-size-3-tablet is-size-5-mobile">
-              <span class="is-size-5-tablet is-size-7-mobile">Home:</span
-              >{{ team.home_city }}
-            </p>
-          </div>
-          <!-- card -->
-        </div>
-        <!-- column -->
-      </div>
-      <!-- columns -->
+      <SelectedCompetitorTeamList
+        :teams="teams"
+        :competitors="competitors"
+        @followTeam="followTeam" />
       <div class="back-or-select-buttons mt-4">
         <BackToPageButton
           @click="selectAgain"
@@ -52,13 +25,15 @@ import CompetitorTeamCount from '../modal/CompetitorTeamCount.vue'
 import CompetitorValidation from '../modal/CompetitorValidation.vue'
 import CompetitorSelectButton from '../Molecules/BooleanButton/CompetitorSelectButton'
 import BackToPageButton from '../atoms/Button/BackToPageButton.vue'
+import SelectedCompetitorTeamList from '../Organism/SelectedCompetitorTeamList.vue'
 
 export default {
   components: {
     CompetitorTeamCount,
     CompetitorValidation,
     CompetitorSelectButton,
-    BackToPageButton
+    BackToPageButton,
+    SelectedCompetitorTeamList
   },
   props: ['teams', 'competitors', 'isShowingMessage'],
   setup(props, context) {
