f2d48dac1b461f104c0ef5aa50ae621b3ce20cfa
R-Tsukada <r.tsukada14@gmail.com>
2022-09-17 14:26:06 +0900

    不必要なコメントアウトを削除

--- app/javascript/head.js
+++ app/javascript/head.js
@@ -1,26 +1,3 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   // ナビゲーションバーガー（navbar-burgerクラスを持つすべての要素）を取得します。
-//   const $navbarBurgers = document.querySelectorAll('.navbar-burger')
-//   console.log($navbarBurgers)
-//   // ナビゲーションバーガーがあるかどうかを確認します。
-//   if ($navbarBurgers.length > 0) {
-//     // すべてのナビゲーションバーガーをループします。
-//     $navbarBurgers.forEach(el => {
-//       // ナビゲーションバーガーにクリックイベントを追加します。
-//       el.addEventListener('click', () => {
-//         // ナビゲーションバーガーのdata-target属性の値を取得します。
-//         const target = el.dataset.target
-//         // メニュー（data-target属性の値をIDとして持つ要素）を取得します。
-//         const $target = document.getElementById(target)
-//         // ナビゲーションバーガーでis-activeクラスを切り替えます。
-//         el.classList.toggle('is-active')
-//         // メニューでis-activeクラスを切り替えます。
-//         $target.classList.toggle('is-active')
-//       })
-//     })
-//   }
-// })
-
 document.addEventListener('DOMContentLoaded', () => {
   // Get all "navbar-burger" elements
   const $navbarBurgers = Array.prototype.slice.call(
