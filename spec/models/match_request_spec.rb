6cb0dde182dd0878f9a4fa5665c1f23a7c43ac10
R-Tsukada <r.tsukada14@gmail.com>
2022-11-16 20:53:03 +0900

    テストを修正する
    外部APIリクエストに適したテストを今後考える必要がある

--- spec/models/match_request_spec.rb
+++ spec/models/match_request_spec.rb
@@ -3,22 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe MatchRequest, type: :model do
-  let(:favorite_url) { URI('https://v3.football.api-sports.io/fixtures?&season=2021&team=42&from=2022-02-30&to=2022-03-30') }
-  let(:competitor_url) do
-    URI('https://v3.football.api-sports.io/fixtures?&season=2021&team=33&from=2022-02-30&to=2022-03-30')
-  end
-  let(:registered_teames) { [favorite_url, competitor_url] }
   let(:matches) { FactoryBot.build(:match) }
 
   it 'is ensure that the MatchRequest.league method is executed' do
     api_matche_mock = double(matches)
-    allow(MatchRequest).to receive(:call).and_return(api_matche_mock)
-    expect { MatchRequest.call }.not_to raise_error
-  end
-
-  it 'is ensure that the MatchRequest.save_match method is executed' do
-    api_match_mock = double(matches)
-    allow(MatchRequest).to receive(:data_save).and_return(api_match_mock)
-    expect { MatchRequest.data_save(registered_teames) }.not_to raise_error
+    allow(MatchRequest.new).to receive(:call).and_return(api_matche_mock)
+    expect { MatchRequest.new.call }.not_to raise_error
   end
 end
--- spec/models/standing_request_spec.rb
+++ spec/models/standing_request_spec.rb
@@ -3,21 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe StandingRequest, type: :model do
-  let(:league) { FactoryBot.build(:league) }
-  let(:favorite_url) { URI("https://v3.football.api-sports.io/standings?league=#{league.api_id}&season=2021&team=42") }
-  let(:competitor_url) { URI("https://v3.football.api-sports.io/standings?league=#{league.api_id}&season=2021&team=33") }
-  let(:registered_teames) { [favorite_url, competitor_url] }
   let(:standings) { FactoryBot.build(:standing) }
 
   it 'is ensure that the StandingRequest.request_response method is executed' do
     standings_mock = double(standings)
-    allow(StandingRequest).to receive(:call).and_return(standings_mock)
-    expect { StandingRequest.call }.not_to raise_error
-  end
-
-  it 'is ensure that the StandingRequest.save_standing method is executed' do
-    standings_mock = double(standings)
-    allow(StandingRequest).to receive(:data_save).and_return(standings_mock)
-    expect { StandingRequest.data_save(registered_teames) }.not_to raise_error
+    allow(StandingRequest.new).to receive(:call).and_return(standings_mock)
+    expect { StandingRequest.new.call }.not_to raise_error
   end
 end
