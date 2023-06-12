1ed1361bc5d17a6a63da9c6cfa8c29ce46b32760
R-Tsukada <r.tsukada14@gmail.com>
2023-06-12 15:30:07 +0900

    CompetitorSelectButtonのテスト作成と微修正をする

---
+++ spec/javascript/components/Molecules/CompetitorSelectButton.spec.js
@@ -0,0 +1,30 @@
+import { mount } from '@vue/test-utils'
+import CompetitorSelectButton from 'Molecules/BooleanButton/CompetitorSelectButton.vue'
+
+describe('CompetitorSelectButton.vue', () => {
+  it('renders router-link and enables DetermineButton when competitors length is between 1 and 3', () => {
+    const wrapper = mount(CompetitorSelectButton, {
+      propsData: {
+        competitors: [{id: 1, name: 'チームA'}, {id: 2, name: 'チームB'}, {id: 3, name: 'チームC'}] // 3 competitors
+      }
+    })
+
+    expect(wrapper.find('router-link').exists()).toBe(true)
+    const determineButton = wrapper.findComponent({ name: 'DetermineButton' })
+    expect(determineButton.exists()).toBe(true)
+    expect(determineButton.props('disabled')).toBe(false)
+  })
+
+  it('disables DetermineButton when competitors length is not between 1 and 3', () => {
+    const wrapper = mount(CompetitorSelectButton, {
+      propsData: {
+        competitors: [] // 0 competitors
+      }
+    })
+
+    expect(wrapper.find('router-link').exists()).toBe(false)
+    const determineButton = wrapper.findComponent({ name: 'DetermineButton' })
+    expect(determineButton.exists()).toBe(true)
+    expect(determineButton.props('disabled')).toBe(true)
+  })
+})
