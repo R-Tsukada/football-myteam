eddf68262d01046f035a49fbf06e24a17440a5b5
R-Tsukada <r.tsukada14@gmail.com>
2024-01-28 14:41:19 +0900

    必要のないパッケージを削除する

--- babel.config.js
+++ babel.config.js
@@ -60,12 +60,6 @@ module.exports = function (api) {
         }
       ],
       [
-        '@babel/plugin-proposal-private-property-in-object',
-        {
-          loose: true
-        }
-      ],
-      [
         '@babel/plugin-transform-runtime',
         {
           helpers: false
