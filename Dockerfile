22ece0d846264bcf5b353e38de0b0471cf46d074
ryo-tsukada <r.tsukada14@gmail.com>
2024-07-30 20:32:41 +0900

    ビルドに必要な対応を実行する

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,8 @@ FROM ruby:3.3.0-bookworm
 
 RUN curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - && \
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-RUN curl -sL https://deb.nodesource.com/setup_20.x | bash -
-RUN apt-get install -y nodejs
-RUN apt-get update -qq && apt-get install -y build-essential libpq-dev postgresql-client yarn
-
-ENV NODE_OPTIONS=--openssl-legacy-provider
+RUN curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs postgresql-client yarn
 
 WORKDIR /football-myteam
 COPY Gemfile /football-myteam/Gemfile
--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,9 +1,32 @@
 const path    = require("path")
 const webpack = require("webpack")
+const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
   mode: "production",
   devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"]
+      }
+    ]
+  },
   entry: {
     application: "./app/javascript/application.js"
   },
@@ -11,11 +34,19 @@ module.exports = {
     filename: "[name].js",
     sourceMapFilename: "[file].map",
     chunkFormat: "module",
-    path: path.resolve(__dirname, "app/assets/builds"),
+    path: path.resolve(__dirname, "..", "..", "app/assets/builds"),
   },
   plugins: [
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
+    new VueLoaderPlugin(),
+    new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: false
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm-bundler.js'
+    },
+    extensions: ['.js', '.vue']
+  }
 }
