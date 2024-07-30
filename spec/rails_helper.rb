eda3bd84fca06daad11f7c977f03bd1b16d84c3d
ryo-tsukada <r.tsukada14@gmail.com>
2024-07-30 21:52:37 +0900

    セレニウムテストが動かなくなっていたため修正する

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,30 +66,31 @@ RSpec.configure do |config|
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Devise::Test::IntegrationHelpers, type: :system
 
-  # ---- Dockerでseleniumテストを実行するときに使用した--------------------------
   Capybara.register_driver :remote_chrome do |app|
     url = 'http://chrome:4444/wd/hub'
-    caps = ::Selenium::WebDriver::Remote::Capabilities.chrome(
-      'goog:chromeOptions' => {
-        'args' => [
-          'no-sandbox',
-          'disable-gpu',
-          'window-size=1680,1050'
-        ]
-      }
+    options = ::Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('no-sandbox')
+    options.add_argument('disable-gpu')
+    options.add_argument('window-size=1680,1050')
+
+    Capybara::Selenium::Driver.new(
+      app,
+      browser: :remote,
+      url: url,
+      capabilities: [options]
     )
-    Capybara::Selenium::Driver.new(app, browser: :remote, url:, capabilities: caps)
   end
 
-  config.before(:each, type: :system) do
-    driven_by :rack_test
-  end
+  RSpec.configure do |config|
+    config.before(:each, type: :system) do
+      driven_by :rack_test
+    end
 
-  config.before(:each, type: :system, js: true) do
-    driven_by :remote_chrome
-    Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
-    Capybara.server_port = 3001
-    Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
+    config.before(:each, type: :system, js: true) do
+      driven_by :remote_chrome
+      Capybara.server_host = '0.0.0.0'
+      Capybara.server_port = 3001
+      Capybara.app_host = "http://web:3001"
+    end
   end
-  # ----------------------------------------------------------------------------
 end
