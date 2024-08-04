38c869f2bd3e7dd4dec40164a8be39b13fdc56ea
ryo-tsukada <r.tsukada14@gmail.com>
2024-08-04 22:04:56 +0900

    activeがtrueのチームだけ表示されるようにする

--- app/controllers/api/team_filter_controller.rb
+++ app/controllers/api/team_filter_controller.rb
@@ -5,6 +5,6 @@ class API::TeamFilterController < ApplicationController
 
   def index
     team = current_user.favorite.team
-    @filter = Team.where(league: team.league_id).where.not(id: team.id)
+    @filter = Team.where(league: team.league_id, active: true).where.not(id: team.id)
   end
 end
--- spec/requests/api/team_filter_spec.rb
+++ spec/requests/api/team_filter_spec.rb
@@ -3,15 +3,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::TeamFilters', type: :request do
+  let!(:league) { FactoryBot.create(:league) }
+  let!(:active_team) { FactoryBot.create(:team, active: true, league:) }
+  let!(:inactive_team) { FactoryBot.create(:team, active: false, league:) }
+  let!(:user_follow_team) { FactoryBot.create(:team, active: true, league:) }
+  let(:user) { FactoryBot.build(:user) }
+
+  before do
+    sign_in user
+    user.favorite_team_follow(user_follow_team)
+  end
+
   describe 'GET /api/team_filter' do
     it 'returns http success' do
-      league = FactoryBot.create(:league)
-      team = FactoryBot.create(:team, league:)
-      user = FactoryBot.build(:user)
-      sign_in user
-      user.favorite_team_follow(team)
       get api_team_filter_index_path(format: :json)
       expect(response).to have_http_status(:success)
+    end
+    it 'returns only active teams' do
+      get '/api/team_filter'
+      expect(response).to have_http_status(:success)
+      json_response = JSON.parse(response.body)
+      expect(json_response.size).to eq(1)
+      expect(json_response.first['id']).to eq(active_team.id)
     end
   end
 end
